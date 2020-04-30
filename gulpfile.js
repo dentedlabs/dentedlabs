f5009ed9bdeec0d4f43c493e664c90fab0d44dfe
Denis Cheung <d.s.h.cheung@gmail.com>
2020-04-30 23:38:18 +0800

    fixed docs/index.html edited gulp to move all dependency package to vendors edited gulp to move free icons to assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
-//
 // Gulpfile
-//
-
 "use strict";
 
 const gulp                   = require('gulp');
@@ -17,23 +14,16 @@ const cache                  = require('gulp-cache');
 const fileinclude            = require('gulp-file-include');
 const browsersync            = require('browser-sync').create();
 
-
-
-//
+// ====================
+// ===== FOR LIVE =====
+// ====================
 // Gulp plumber error handler - displays if any error occurs during the process on your command
-//
-
 function errorLog(error) {
   console.error.bind(error);
   this.emit('end');
 }
 
-
-
-//
 // SASS - Compile SASS files into CSS
-//
-
 function scss() {
   return gulp
     .src('./assets/scss/**/*.scss')
@@ -55,12 +45,7 @@ function scss() {
     .pipe(browsersync.stream());
 }
 
-
-
-//
 // BrowserSync (live reload) - keeps multiple browsers & devices in sync when building websites
-//
-
 function browserSync(done) {
   browsersync.init({
     files: "./*.html",
@@ -85,12 +70,7 @@ function browserSyncReload(done) {
   done();
 }
 
-
-
-//
 // Gulp Watch and Tasks
-//
-
 function watch() {
   gulp.watch('./assets/scss/**/*.scss', scss);
   gulp.watch(
@@ -105,16 +85,42 @@ function watch() {
   // gulp.watch('./documentation/partials/**/*.html', fileIncludeDocs);
 }
 
-// Gulp Tasks
-// gulp.task('default', gulp.parallel(watch, scss, browserSync, fileInclude, fileIncludeDocs));
+// File Include
+function fileInclude() {
+  return gulp
+    .src(['./snippets/partials/src/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+      indent: true
+    }))
+    .pipe(gulp.dest('./snippets/'))
+    .pipe(browsersync.stream());
+};
+
+function fileIncludeDocs() {
+  return gulp
+    .src(['./documentation/partials/src/**/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+      indent: true
+    }))
+    .pipe(gulp.dest('./documentation/'))
+    .pipe(browsersync.stream());
+};
+
+// Gulp Live Tasks
 gulp.task('default', gulp.parallel(watch, scss, browserSync));
+// gulp.task('default', gulp.parallel(watch, scss, browserSync, fileInclude, fileIncludeDocs));
+// gulp.task('fileInclude', fileInclude);
+// gulp.task('fileIncludeDocs', fileIncludeDocs);
 
 
-
-//
+// ====================
+// ===== FOR DIST =====
+// ====================
 // CSS minifier - merges and minifies the below given list of Space libraries into one theme.min.css
-//
-
 function minCSS() {
   return gulp
     .src([
@@ -125,12 +131,7 @@ function minCSS() {
     .pipe(gulp.dest('./docs/assets/css/'));
 }
 
-
-
-//
 // JavaSript minifier - merges and minifies the below given list of Space libraries into one theme.min.js
-//
-
 function minJS() {
   return gulp
     .src([
@@ -159,12 +160,7 @@ function minJS() {
     .pipe(gulp.dest('./docs/assets/js/'));
 }
 
-
-
-//
 // Copy Vendors - a utility to copy client-side dependencies into a folder
-//
-
 function copyVendors() {
   return gulp
     .src([
@@ -183,7 +179,9 @@ function copyVendors() {
       './node_modules/*jquery-migrate/**/*',
       './node_modules/*jquery-validation/**/*',
       './node_modules/*leaflet/**/*',
+      './node_modules/*popper.js/**/*',
       './node_modules/*pwstrength-bootstrap/**/*',
+      './node_modules/*slick-carousel/**/*',
       './node_modules/*summernote/**/*',
       './node_modules/*tagify/**/*',
       './node_modules/*typed.js/**/*',
@@ -191,41 +189,92 @@ function copyVendors() {
     .pipe(gulp.dest('./docs/assets/vendor/'))
 };
 
-
-
-//
-// File Include
-//
-
-function fileInclude() {
+// Copy Vendors - copy theme specific vendors that is not not public available online
+function copyHSVendors() {
   return gulp
-    .src(['./snippets/partials/src/*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file',
-      indent: true
-    }))
-    .pipe(gulp.dest('./snippets/'))
-    .pipe(browsersync.stream());
+    .src([
+      './assets/vendor/*hs-counter/**/*',
+      './assets/vendor/*hs-file-attach/**/*',
+      './assets/vendor/*hs-go-to/**/*',
+      './assets/vendor/*hs-header/**/*',
+      './assets/vendor/*hs-header-fullscreen/**/*',
+      './assets/vendor/*hs-mega-menu/**/*',
+      './assets/vendor/*hs-megamenu/**/*',
+      './assets/vendor/*hs-progress-bar/**/*',
+      './assets/vendor/*hs-quantity-counter/**/*',
+      './assets/vendor/*hs-scroll-nav/**/*',
+      './assets/vendor/*hs-scroll-to-in-overflowed-container/**/*',
+      './assets/vendor/*hs-show-animation/**/*',
+      './assets/vendor/*hs-step-form/**/*',
+      './assets/vendor/*hs-sticky-block/**/*',
+      './assets/vendor/*hs-switch/**/*',
+      './assets/vendor/*hs-toggle-state/**/*',
+      './assets/vendor/*hs-toggle-switch/**/*',
+      './assets/vendor/*hs-unfold/**/*',
+      './assets/vendor/*hs-video-bg/**/*',
+      './assets/vendor/*hs-video-player/**/*',
+    ])
+    .pipe(gulp.dest('./docs/assets/vendor/'))
+}
+
+// Copy Vendors - no substitute - copy vendors that does not have substitute
+function copyNoSubVendors() {
+  return gulp
+    .src([
+      './assets/vendor/*chart.js.extensions/**/*',
+      './assets/vendor/*circles/**/*',
+      './assets/vendor/*cubeportfolio/**/*',
+      './assets/vendor/*dzsparallaxer/**/*',
+      './assets/vendor/appear.js',
+      './assets/vendor/polifills.js',
+    ])
+    .pipe(gulp.dest('./docs/assets/vendor/'))
+}
+
+// Copy Vendors - tagify - rename @yaireo/tagify to tagify
+function copyTagify() {
+  return gulp
+    .src([
+      './node_modules/@yaireo/tagify/**/*',
+    ])
+    .pipe(gulp.dest('./docs/assets/vendor/tagify'))
 };
 
-// function fileIncludeDocs() {
-//   return gulp
-//     .src(['./documentation/partials/src/**/*.html'])
-//     .pipe(fileinclude({
-//       prefix: '@@',
-//       basepath: '@file',
-//       indent: true
-//     }))
-//     .pipe(gulp.dest('./documentation/'))
-//     .pipe(browsersync.stream());
-// };
+// Copy Vendors - fancybox - rename @fancyapps/fancybox to fancybox
+function copyFancybox() {
+  return gulp
+    .src([
+      './node_modules/@fancyapps/fancybox/**/*',
+    ])
+    .pipe(gulp.dest('./docs/assets/vendor/fancybox'))
+};
 
+// Copy Vendors - font-awesome - rename @fortawesome/fontawesome-free to font-awesome
+function copyFontAwesome() {
+  return gulp
+    .src([
+      './node_modules/@fortawesome/fontawesome-free/**/*',
+    ])
+    .pipe(gulp.dest('./docs/assets/vendor/font-awesome'))
+};
 
+// Copy SVG
+function copySVG() {
+  return gulp
+    .src([
+      './assets/svg/*icons/**/*',
+    ])
+    .pipe(gulp.dest('./docs/assets/svg/'))
+};
 
+// Gulp Dist Tasks
 gulp.task('minCSS', minCSS);
 gulp.task('minJS', minJS);
 gulp.task('copyVendors', copyVendors);
-// gulp.task('fileInclude', fileInclude);
-// gulp.task('fileIncludeDocs', fileIncludeDocs);
-gulp.task('dist', gulp.series(copyVendors, minCSS, minJS));
+gulp.task('copyHSVendors', copyHSVendors);
+gulp.task('copyNoSubVendors', copyNoSubVendors);
+gulp.task('copyTagify', copyTagify);
+gulp.task('copyFancybox', copyFancybox);
+gulp.task('copyFontAwesome', copyFontAwesome);
+gulp.task('copySVG', copySVG);
+gulp.task('dist', gulp.series(minCSS, minJS, copyVendors, copyHSVendors, copyNoSubVendors, copyTagify, copyFontAwesome, copySVG));
