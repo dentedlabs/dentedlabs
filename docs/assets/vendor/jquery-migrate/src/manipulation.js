3095fbc5e67a51405ca0bd94c47b22dbeac2371d
Gram <g@dented.io>
2025-05-25 01:10:44 -0400

    updated gh-pages

--- docs/assets/vendor/jquery-migrate/src/manipulation.js
+++
@@ -1,7 +0,0 @@
-var rxhtmlTag = /<(?!area|br|col|embed|hr|img|input|link|meta|param)(([a-z][^\/\0>\x20\t\r\n\f]*)[^>]*)\/>/gi;
-
-jQuery.UNSAFE_restoreLegacyHtmlPrefilter = function() {
-	jQuery.htmlPrefilter = function( html ) {
-		return html.replace( rxhtmlTag, "<$1></$2>" );
-	};
-};
